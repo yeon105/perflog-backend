4716caacc41348bc6caf5fc54f0311b73fb8c873
Heeyeon An <gmldus4613@naver.com>
2025-10-17 03:19:07 +0900

    feat(preference): 향수 선호 분석을 위한 메서드 추가

--- src/main/kotlin/com/perflog/domain/perfume/repository/PerfumeTagRepository.kt
+++ src/main/kotlin/com/perflog/domain/perfume/repository/PerfumeTagRepository.kt
@@ -2,6 +2,7 @@ package com.perflog.domain.perfume.repository
 
 import com.perflog.domain.perfume.model.entity.Perfume
 import com.perflog.domain.perfume.model.entity.PerfumeTag
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
@@ -13,4 +14,27 @@ interface PerfumeTagRepository : JpaRepository<PerfumeTag, Long> {
     @Modifying
     @Query("delete from PerfumeTag pt where pt.perfume.id = :perfumeId")
     fun deleteByPerfumeId(@Param("perfumeId") perfumeId: Long)
+
+    @Query(
+        """
+    select t.id
+    from PerfumeTag pt
+    join pt.tag t
+    where pt.perfume.id in :perfumeIds
+    group by t.id
+    order by count(pt.id) desc
+"""
+    )
+    fun findTopTagsByPerfumeIds(perfumeIds: List<Long>, pageable: Pageable): List<Long>
+
+    @Query(
+        """
+    select pt.perfume
+    from PerfumeTag pt
+    where pt.tag.id IN :tagIds
+    group by pt.perfume
+    having count(pt.tag.id) >= 2
+"""
+    )
+    fun findPerfumesByMatchingTags(tagIds: List<Long>): List<Perfume>
 }
