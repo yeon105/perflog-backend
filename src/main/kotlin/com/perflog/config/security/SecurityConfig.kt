98ad4fd0ac0d8ae1dd802a07afdf7147dd714f87
Heeyeon An <gmldus4613@naver.com>
2025-10-16 17:26:14 +0900

    chore(security): /api/perfumes 중복 경로 제거

--- src/main/kotlin/com/perflog/config/security/SecurityConfig.kt
+++ src/main/kotlin/com/perflog/config/security/SecurityConfig.kt
@@ -50,10 +50,7 @@ class SecurityConfig(
                     "/", "/api/member/login", "/api/member/refresh"
                 ).permitAll()
                     .requestMatchers(
-                        HttpMethod.GET,
-                        "/api/perfumes",
-                        "/api/perfumes/**",
-                        "/api/reviews/perfume/**"
+                        HttpMethod.GET, "/api/perfumes/**", "/api/reviews/perfume/**"
                     ).permitAll()
                     .requestMatchers(HttpMethod.POST, "/api/member").permitAll()
 
