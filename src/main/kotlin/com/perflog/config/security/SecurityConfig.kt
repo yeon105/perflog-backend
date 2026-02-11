256ad0401a9cda6c98aabe3992279347ca530d08
RudySeo <gun100491@gmail.com>
2026-02-11 20:08:53 +0900

    feat(securityFilterChain): search 필터추가

--- src/main/kotlin/com/perflog/config/security/SecurityConfig.kt
+++ src/main/kotlin/com/perflog/config/security/SecurityConfig.kt
@@ -47,7 +47,7 @@ class SecurityConfig(
             .httpBasic { httpBasic -> httpBasic.disable() }
             .authorizeHttpRequests { auth ->
                 auth.requestMatchers(
-                    "/", "/api/member/login", "/api/member/refresh"
+                    "/", "/api/member/login", "/api/member/refresh", "/api/search/**",
                 ).permitAll()
                     .requestMatchers(
                         HttpMethod.GET,
