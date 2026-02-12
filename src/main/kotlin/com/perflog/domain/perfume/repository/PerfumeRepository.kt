42d43c0e91e8d035a5dee7c69d1fd489b02baf80
RudySeo <gun100491@gmail.com>
2026-02-12 14:33:11 +0900

    remove(PerfumeRepository): 검색 메서드 삭제

--- src/main/kotlin/com/perflog/domain/perfume/repository/PerfumeRepository.kt
+++ src/main/kotlin/com/perflog/domain/perfume/repository/PerfumeRepository.kt
@@ -1,8 +1,6 @@
 package com.perflog.domain.perfume.repository
 
 import com.perflog.domain.perfume.model.entity.Perfume
-import org.springframework.data.domain.Page
-import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.JpaRepository
 import java.util.*
@@ -13,13 +11,5 @@ interface PerfumeRepository : JpaRepository<Perfume, Long> {
     @EntityGraph(attributePaths = ["perfumeTags", "perfumeTags.tag"])
     fun findWithTagsById(id: Long): Optional<Perfume>
 
-    fun findByNameContainingIgnoreCase(name: String, pageable: Pageable): Page<Perfume>
 
-    fun findByBrandContainingIgnoreCase(brand: String, pageable: Pageable): Page<Perfume>
-
-    fun findByNameContainingIgnoreCaseOrBrandContainingIgnoreCase(
-        name: String,
-        brand: String,
-        pageable: Pageable
-    ): Page<Perfume>
 }
