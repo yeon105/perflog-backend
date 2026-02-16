e2f02aa7fcbee00461acf6cb46c0e68a48b60597
RudySeo <gun100491@gmail.com>
2026-02-16 20:38:50 +0900

    feat(PerfumeDocument): PerfumeDocument 추가

--- src/main/kotlin/com/perflog/domain/perfume/model/document/PerfumeDocument.kt
+++ src/main/kotlin/com/perflog/domain/perfume/model/document/PerfumeDocument.kt
@@ -1,28 +1,60 @@
 package com.perflog.domain.perfume.model.document
 
 import com.perflog.domain.perfume.model.entity.Perfume
+import com.perflog.domain.perfume.model.entity.Tag
+import org.springframework.data.annotation.Id
+import org.springframework.data.elasticsearch.annotations.Document
+import org.springframework.data.elasticsearch.annotations.Field
+import org.springframework.data.elasticsearch.annotations.FieldType
 
+@Document(indexName = "perfumes")
 data class PerfumeDocument(
-    val id: Long,
-    val name: String,
-    val brand: String,
-    val season: String,
-    val gender: String,
-    val imageUrl: String,
-) {
 
+    @Id
+    val id: String,
+
+    @Field(type = FieldType.Text)
+    val name: String,
+
+    @Field(type = FieldType.Text)
+    val brand: String,
+
+    @Field(type = FieldType.Integer)
+    val launchYear: Int?,
+
+    @Field(type = FieldType.Keyword)
+    val season: String?,
+
+    @Field(type = FieldType.Keyword)
+    val gender: String?,
+
+    @Field(type = FieldType.Text)
+    val notes: String?,
+
+    @Field(type = FieldType.Keyword)
+    val tags: List<String>
+) {
     companion object {
 
-        fun from(perfume: Perfume): PerfumeDocument {
+        fun of(
+            perfume: Perfume,
+            tags: List<Tag>
+        ): PerfumeDocument {
+
             return PerfumeDocument(
-                id = perfume.id,
+                id = perfume.id.toString(),
                 name = perfume.name,
                 brand = perfume.brand,
+                launchYear = perfume.launchYear,
                 season = perfume.season.name,
                 gender = perfume.gender.name,
-                imageUrl = perfume.imageUrl,
-
-                )
+                notes = listOfNotNull(
+                    perfume.topNotes,
+                    perfume.middleNotes,
+                    perfume.baseNotes
+                ).joinToString(" "),
+                tags = tags.map { it.name }
+            )
         }
     }
 }
