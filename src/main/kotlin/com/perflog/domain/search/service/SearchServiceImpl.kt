e8af445520e4c5f9dc5bddcb0fbaf68c513de7d8
RudySeo <gun100491@gmail.com>
2026-02-17 23:07:23 +0900

    feat(setting): 자동완성 추가

--- src/main/kotlin/com/perflog/domain/search/service/SearchServiceImpl.kt
+++ src/main/kotlin/com/perflog/domain/search/service/SearchServiceImpl.kt
@@ -1,15 +1,19 @@
 package com.perflog.domain.search.service
 
+import com.perflog.domain.perfume.dto.PerfumeDto
 import com.perflog.domain.perfume.model.document.PerfumeDocument
 import com.perflog.domain.perfume.model.enum.SearchTarget
 import com.perflog.domain.perfume.repository.PerfumeSearchRepository
+import org.springframework.data.elasticsearch.client.elc.NativeQuery
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
 @Transactional(readOnly = true)
 class SearchServiceImpl(
-    private val perfumeSearchRepository: PerfumeSearchRepository
+    private val perfumeSearchRepository: PerfumeSearchRepository,
+    private val elasticsearchOperations: ElasticsearchOperations
 ) : SearchService {
 
     override fun searchPerfume(
@@ -24,5 +28,29 @@ class SearchServiceImpl(
                 keyword, keyword, keyword
             )
         }
+    }
+
+    override fun autocomplete(keyword: String): List<PerfumeDto.autocomplete> {
+        try {
+
+            val query = NativeQuery.builder()
+                .withQuery { q ->
+                    q.match { m ->
+                        m.field("name")
+                            .query(keyword)
+                    }
+                }
+                .withMaxResults(5)
+                .build()
+
+            val searchHits =
+                elasticsearchOperations.search(query, PerfumeDocument::class.java)
+            return searchHits.searchHits
+                .map { PerfumeDto.autocomplete(name = it.content.name) }
+
+        } catch (e: Exception) {
+            println(e.message)
+        }
+        return emptyList()
     }
 }
