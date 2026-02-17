789bb7f8d02db0601663aa7b39afd79acd9ab952
RudySeo <gun100491@gmail.com>
2026-02-17 23:07:03 +0900

    feat(setting): 엘라스틱 추가

--- src/main/kotlin/com/perflog/domain/perfume/model/document/PerfumeDocument.kt
+++ src/main/kotlin/com/perflog/domain/perfume/model/document/PerfumeDocument.kt
@@ -1,19 +1,27 @@
 package com.perflog.domain.perfume.model.document
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.perflog.domain.perfume.model.entity.Perfume
 import com.perflog.domain.perfume.model.entity.Tag
 import org.springframework.data.annotation.Id
 import org.springframework.data.elasticsearch.annotations.Document
 import org.springframework.data.elasticsearch.annotations.Field
 import org.springframework.data.elasticsearch.annotations.FieldType
+import org.springframework.data.elasticsearch.annotations.Setting
 
 @Document(indexName = "perfumes")
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Setting(settingPath = "elasticsearch/perfume-settings.json")
 data class PerfumeDocument(
 
     @Id
     val id: String,
 
-    @Field(type = FieldType.Text)
+    @Field(
+        type = FieldType.Text,
+        analyzer = "autocomplete_analyzer",
+        searchAnalyzer = "autocomplete_search_analyzer"
+    )
     val name: String,
 
     @Field(type = FieldType.Text)
@@ -32,8 +40,10 @@ data class PerfumeDocument(
     val notes: String?,
 
     @Field(type = FieldType.Keyword)
-    val tags: List<String>
-) {
+    val tags: List<String>,
+
+
+    ) {
     companion object {
 
         fun of(
