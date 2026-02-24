35091b95227f1a6763cb82b540b032993a23c489
RudySeo <gun100491@gmail.com>
2026-02-24 20:12:28 +0900

    feat(Kafka): KafkaConsumerConfig 설정

--- src/main/kotlin/com/perflog/config/Kafka/KafkaConsumerConfig.kt
+++ src/main/kotlin/com/perflog/config/Kafka/KafkaConsumerConfig.kt
@@ -7,7 +7,12 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
 import org.springframework.kafka.core.ConsumerFactory
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.listener.ContainerProperties
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer
+import org.springframework.kafka.listener.DefaultErrorHandler
 import org.springframework.kafka.support.serializer.JsonDeserializer
+import org.springframework.util.backoff.FixedBackOff
 
 @Configuration
 class KafkaConsumerConfig {
@@ -21,6 +26,8 @@ class KafkaConsumerConfig {
             ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG to "localhost:9092",
             ConsumerConfig.GROUP_ID_CONFIG to "perfume-group",
             ConsumerConfig.AUTO_OFFSET_RESET_CONFIG to "earliest",
+            //   autoCommit 수정
+            ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG to false,
             ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG to StringDeserializer::class.java,
             ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG to deserializer
         )
@@ -30,14 +37,23 @@ class KafkaConsumerConfig {
 
     @Bean
     fun kafkaListenerContainerFactory(
-        consumerFactory: ConsumerFactory<String, PerfumeCreatedEvent>
+        consumerFactory: ConsumerFactory<String, PerfumeCreatedEvent>,
+        kafkaTemplate: KafkaTemplate<String, Any>
     ): ConcurrentKafkaListenerContainerFactory<String, PerfumeCreatedEvent> {
 
         val factory =
             ConcurrentKafkaListenerContainerFactory<String, PerfumeCreatedEvent>()
 
         factory.consumerFactory = consumerFactory
-        factory.setConcurrency(3)
+        // MANUAL
+        factory.containerProperties.ackMode =
+            ContainerProperties.AckMode.RECORD
+
+        // DLQ  재시도 설정
+        val recoverer = DeadLetterPublishingRecoverer(kafkaTemplate)
+        val backOff = FixedBackOff(1000L, 9) //
+
+        factory.setCommonErrorHandler(DefaultErrorHandler(recoverer, backOff))
 
         return factory
     }
